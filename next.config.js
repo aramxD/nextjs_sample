373f2866b194dcef2333c63ca00105b84bb5c720
julio santacruz <aram.jsh1@gmail.com>
2022-10-13 21:32:51 -0700

    implementacion estilos

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  // swcMinify: true,
+  // env:{
+  //   customKey:'customValue'
+  // },
+  // basePath: '/dist',
+  // compress:true,
+  // async redirects({
+  //   return [
+  //     {
+  //       source: '/hola',
+  //       destination: '/hello',
+  //       permanentl: true,
+  //     }
+  //   ]
+  // })
 }
 
 module.exports = nextConfig
