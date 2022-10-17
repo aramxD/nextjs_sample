53d48c42768bbc67b13bb8fda1bc7547df8a80f9
julio santacruz <aram.jsh1@gmail.com>
2022-10-16 22:46:54 -0700

    ajuste pwa

--- next.config.js
+++ next.config.js
@@ -1,23 +1,43 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['placeimg.com','api.escuelajs.co', 'api.lorem.space','wixmp.com', 'romapy']},
-  // env:{
-  //   customKey:'customValue'
-  // },
-  // basePath: '/dist',
-  // compress:true,
-  // async redirects({
-  //   return [
-  //     {
-  //       source: '/hola',
-  //       destination: '/hello',
-  //       permanentl: true,
-  //     }
-  //   ]
-  // })
-}
+    domains: ['placeimg.com','api.escuelajs.co', 'api.lorem.space','wixmp.com', 'romapy'],
+  },
+};
 
-module.exports = nextConfig
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  include: ['production'],
+  register: true,
+});
+
+module.exports = withPWA(nextConfig);
+
+
+
+// Configuracion normal 
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   images: {
+//     domains: ['placeimg.com','api.escuelajs.co', 'api.lorem.space','wixmp.com', 'romapy']},
+//   // env:{
+//   //   customKey:'customValue'
+//   // },
+//   // basePath: '/dist',
+//   // compress:true,
+//   // async redirects({
+//   //   return [
+//   //     {
+//   //       source: '/hola',
+//   //       destination: '/hello',
+//   //       permanentl: true,
+//   //     }
+//   //   ]
+//   // })
+// }
+
+// module.exports = nextConfig
